6e3935351e2c15e2b0b4ace2f963eb1955f78ab9
ig1711 <813shihab@gmail.com>
2025-10-23 15:07:06 +0600

    implement basic ui with egui using glow instead of gl_generator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{num::NonZeroU32, rc::Rc};
 
+use egui::{Color32, Frame};
 use glutin::{
     config::{ConfigTemplateBuilder, GlConfig},
     context::PossiblyCurrentContext,
@@ -8,12 +9,13 @@ use glutin::{
     surface::{GlSurface, Surface, SurfaceAttributesBuilder, SwapInterval, WindowSurface},
 };
 
+use egui_winit::winit;
+
 use winit::{
     application::ApplicationHandler,
     dpi::LogicalSize,
-    event::{ElementState, KeyEvent, WindowEvent},
+    event::WindowEvent,
     event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy},
-    keyboard::{Key, NamedKey},
     raw_window_handle::{HasDisplayHandle, HasWindowHandle},
     window::{Window, WindowId},
 };
@@ -37,8 +39,6 @@ use libmpv2::{
 };
 
 use std::ffi::{CString, c_void};
-
-use ab_glyph::{point, Font, FontRef, Glyph};
 
 type GlContext = Rc<Display>;
 
@@ -109,7 +109,7 @@ fn main() {
 }
 
 struct App {
-    renderer: Option<Renderer>,
+    egui_glow: Option<egui_glow::EguiGlow>,
     gl_surface: Option<Surface<WindowSurface>>,
     gl_context: Option<PossiblyCurrentContext>,
     window: Option<Window>,
@@ -117,12 +117,13 @@ struct App {
     mpv_renderer: Option<RenderContext>,
     mpv: Option<Mpv>,
     elp: EventLoopProxy<UserEvent>,
+    text: String,
 }
 
 impl App {
     fn new(sender: mpsc::Sender<String>, elp: EventLoopProxy<UserEvent>) -> Self {
         App {
-            renderer: None,
+            egui_glow: None,
             gl_surface: None,
             gl_context: None,
             window: None,
@@ -130,6 +131,7 @@ impl App {
             mpv_renderer: None,
             mpv: None,
             elp,
+            text: "edit text".to_owned(),
         }
     }
 }
@@ -174,6 +176,7 @@ enum Command {
 enum UserEvent {
     IpcMessage(IpcData),
     MpvRedrawRequested,
+    EguiRedraw,
 }
 
 impl ApplicationHandler<UserEvent> for App {
@@ -271,8 +274,29 @@ impl ApplicationHandler<UserEvent> for App {
         self.gl_context = Some(possibly_current_context);
         self.gl_surface = Some(gl_surface);
 
-        self.renderer
-            .get_or_insert_with(|| Renderer::new(&gl_config.display()));
+        let gl = unsafe {
+            glow::Context::from_loader_function(|s| {
+                let s = std::ffi::CString::new(s)
+                    .expect("failed to construct C string from string for gl proc address");
+
+                gl_display.get_proc_address(&s)
+            })
+        };
+
+        let gl = std::sync::Arc::new(gl);
+
+        let egui_glow = egui_glow::EguiGlow::new(event_loop, gl.clone(), None, None, true);
+
+        let elp3 = self.elp.clone();
+        egui_glow
+            .egui_ctx
+            .set_request_repaint_callback(move |_info| {
+                elp3.clone()
+                    .send_event(UserEvent::EguiRedraw)
+                    .expect("Cannot send event");
+            });
+
+        self.egui_glow = Some(egui_glow);
 
         self.mpv = Some(
             Mpv::with_initializer(|init| {
@@ -332,45 +356,107 @@ impl ApplicationHandler<UserEvent> for App {
                     NonZeroU32::new(size.width).unwrap(),
                     NonZeroU32::new(size.height).unwrap(),
                 );
-
-                let renderer = self.renderer.as_ref().unwrap();
-                renderer.resize(size.width as i32, size.height as i32);
             }
             WindowEvent::RedrawRequested => {
-                let gl_surface = self.gl_surface.as_ref().unwrap();
-                // let window = self.window.as_ref().unwrap();
-                let gl_context = self.gl_context.as_ref().unwrap();
-                let renderer = self.renderer.as_ref().unwrap();
-                renderer.draw();
-                // window.request_redraw();
-
-                gl_surface.swap_buffers(gl_context).unwrap();
-            }
-
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        logical_key: Key::Named(NamedKey::Space),
-                        state: ElementState::Released,
-                        ..
-                    },
-                ..
-            } => {
-                let msg = Message::new("ryo", "chat msg");
-
-                self.sender
-                    .blocking_send(serde_json::to_string(&IpcData::Message(msg)).unwrap())
-                    .unwrap();
-
-                let path = "./video.mp4";
-                self.mpv
+                let Self { text, .. } = self;
+                self.egui_glow
                     .as_mut()
                     .unwrap()
-                    .command("loadfile", &[&path, "replace"])
-                    .unwrap();
+                    .run(self.window.as_mut().unwrap(), |egui_ctx| {
+                        egui::SidePanel::left("my_side_panel")
+                            .frame(Frame::new().fill(Color32::from_rgba_unmultiplied(0, 0, 0, 0)))
+                            .show(egui_ctx, |ui| {
+                                ui.heading("Hello World!");
+                                ui.heading("text");
+                                egui::TextEdit::multiline(text)
+                                    .hint_text("Type something!")
+                                    .show(ui);
+
+                                if ui.button("Send to nodejs").clicked() {
+                                    let msg = Message {
+                                        username: "ryo".to_string(),
+                                        content: text.to_owned(),
+                                    };
+
+                                    self.sender
+                                        .blocking_send(
+                                            serde_json::to_string(&IpcData::Message(msg)).unwrap(),
+                                        )
+                                        .unwrap();
+                                }
+
+                                if ui.button("Quit").clicked() {
+                                    println!("clicked");
+                                    event_loop.exit();
+                                }
+
+                                if ui.button("play video").clicked() {
+                                    let path = "./video.mp4";
+                                    self.mpv
+                                        .as_mut()
+                                        .unwrap()
+                                        .command("loadfile", &[&path, "replace"])
+                                        .unwrap();
+                                };
+
+                                // ui.color_edit_button_rgb(self.clear_color.as_mut().try_into().unwrap());
+                            });
+                    });
+
+                let gl_surface = self.gl_surface.as_ref().unwrap();
+                let window = self.window.as_ref().unwrap();
+                let gl_context = self.gl_context.as_ref().unwrap();
+
+                let size = window.inner_size();
+
+                self.mpv_renderer
+                    .as_mut()
+                    .unwrap()
+                    .render::<GlContext>(0, size.width as _, size.height as _, true)
+                    .expect("Failed to draw on sdl2 window");
+
+                self.egui_glow
+                    .as_mut()
+                    .unwrap()
+                    .paint(self.window.as_mut().unwrap());
+
+                gl_surface.swap_buffers(gl_context).unwrap();
+                // window.request_redraw();
             }
 
+            // WindowEvent::KeyboardInput {
+            //     event:
+            //         KeyEvent {
+            //             logical_key: Key::Named(NamedKey::Space),
+            //             state: ElementState::Released,
+            //             ..
+            //         },
+            //     ..
+            // } => {
+            //     let msg = Message::new("ryo", "chat msg");
+
+            //     self.sender
+            //         .blocking_send(serde_json::to_string(&IpcData::Message(msg)).unwrap())
+            //         .unwrap();
+
+            //     let path = "./video.mp4";
+            //     self.mpv
+            //         .as_mut()
+            //         .unwrap()
+            //         .command("loadfile", &[&path, "replace"])
+            //         .unwrap();
+            // }
             _ => (),
+        }
+
+        let event_response = self
+            .egui_glow
+            .as_mut()
+            .unwrap()
+            .on_window_event(self.window.as_mut().unwrap(), &event);
+
+        if event_response.repaint {
+            self.window.as_mut().unwrap().request_redraw();
         }
     }
 
@@ -387,347 +473,12 @@ impl ApplicationHandler<UserEvent> for App {
                 _ => (),
             },
             UserEvent::MpvRedrawRequested => {
-                let gl_surface = self.gl_surface.as_ref().unwrap();
-                let window = self.window.as_ref().unwrap();
-                let gl_context = self.gl_context.as_ref().unwrap();
+                self.window.as_ref().unwrap().request_redraw();
+            }
 
-                let size = window.inner_size();
-
-                self.mpv_renderer
-                    .as_mut()
-                    .unwrap()
-                    .render::<GlContext>(0, size.width as _, size.height as _, true)
-                    .expect("Failed to draw on sdl2 window");
-
-                let renderer = self.renderer.as_ref().unwrap();
-                renderer.draw();
-
-                gl_surface.swap_buffers(gl_context).unwrap();
+            UserEvent::EguiRedraw => {
+                self.window.as_ref().unwrap().request_redraw();
             }
         }
     }
 }
-
-use std::ffi::CStr;
-use std::ops::Deref;
-
-pub mod gl {
-    #![allow(clippy::all)]
-    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
-}
-
-// use gl::types::GLfloat;
-
-pub struct Renderer {
-    program: gl::types::GLuint,
-    vao: gl::types::GLuint,
-    vbo: gl::types::GLuint,
-    gl: gl::Gl,
-    texture: gl::types::GLuint,
-}
-
-impl Renderer {
-    pub fn new<D: GlDisplay>(gl_display: &D) -> Self {
-        unsafe {
-            let gl = gl::Gl::load_with(|symbol| {
-                let symbol = CString::new(symbol).unwrap();
-                gl_display.get_proc_address(symbol.as_c_str()).cast()
-            });
-
-            if let Some(renderer) = get_gl_string(&gl, gl::RENDERER) {
-                println!("Running on {}", renderer.to_string_lossy());
-            }
-            if let Some(version) = get_gl_string(&gl, gl::VERSION) {
-                println!("OpenGL Version {}", version.to_string_lossy());
-            }
-
-            if let Some(shaders_version) = get_gl_string(&gl, gl::SHADING_LANGUAGE_VERSION) {
-                println!("Shaders version on {}", shaders_version.to_string_lossy());
-            }
-
-            let vertex_shader = create_shader(&gl, gl::VERTEX_SHADER, VERTEX_SHADER_SOURCE);
-            let fragment_shader = create_shader(&gl, gl::FRAGMENT_SHADER, FRAGMENT_SHADER_SOURCE);
-
-            let program = gl.CreateProgram();
-
-            gl.AttachShader(program, vertex_shader);
-            gl.AttachShader(program, fragment_shader);
-
-            gl.LinkProgram(program);
-
-            gl.UseProgram(program);
-
-            gl.DeleteShader(vertex_shader);
-            gl.DeleteShader(fragment_shader);
-
-            // gl.Enable(gl::CULL_FACE);
-            gl.Enable(gl::BLEND);
-            gl.BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
-
-            gl.PixelStorei(gl::UNPACK_ALIGNMENT, 1);
-
-
-            let font = FontRef::try_from_slice(include_bytes!("/usr/share/fonts/TTF/FiraCode-Regular.ttf")).unwrap();
-            
-            let q_glyph: Glyph = font
-            .glyph_id('A')
-            .with_scale_and_position(600.0, point(0.0, 0.0));
-        
-            let mut texture = std::mem::zeroed();
-            
-            font.outline_glyph(q_glyph).map(|outlined| {
-                let px_bounds = outlined.px_bounds();
-                let width = px_bounds.width().ceil() as u32;
-                let height = px_bounds.height().ceil() as u32;
-
-                let mut image = image::GrayImage::new(width, height);
-
-                outlined.draw(|x, y, val| image.put_pixel(x, y, image::Luma([(val * 255.) as u8])));
-                
-                image.reverse();
-                
-                gl.GenTextures(1, &mut texture);
-                gl.BindTexture(gl::TEXTURE_2D, texture);
-                gl.TexImage2D(
-                    gl::TEXTURE_2D,
-                    0,
-                    gl::RED as _,
-                    width as _,
-                    height as _,
-                    0,
-                    gl::RED,
-                    gl::UNSIGNED_BYTE,
-                    image.as_ptr() as *const _,
-                );
-            });
-
-            gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as _);
-            gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as _);
-            gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER,gl::LINEAR as _);
-            gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as _);
-            
-
-
-            let mut vao = std::mem::zeroed();
-            gl.GenVertexArrays(1, &mut vao);
-            gl.BindVertexArray(vao);
-
-            let mut vbo = std::mem::zeroed();
-            gl.GenBuffers(1, &mut vbo);
-            gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl.BufferData(
-                gl::ARRAY_BUFFER,
-                (24 * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-                // (VERTEX_DATA.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-                std::ptr::null(),
-                // VERTEX_DATA.as_ptr() as *const _,
-                gl::DYNAMIC_DRAW,
-            );
-
-            // let pos_attrib = gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
-            // let color_attrib = gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
-            // gl.VertexAttribPointer(
-            //     pos_attrib as gl::types::GLuint,
-            //     2,
-            //     gl::FLOAT,
-            //     0,
-            //     5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
-            //     std::ptr::null(),
-            // );
-
-            gl.VertexAttribPointer(
-                0 as gl::types::GLuint,
-                4,
-                gl::FLOAT,
-                0,
-                4 * std::mem::size_of::<f32>() as gl::types::GLsizei,
-                std::ptr::null(),
-            );
-
-            // gl.VertexAttribPointer(
-            //     color_attrib as gl::types::GLuint,
-            //     3,
-            //     gl::FLOAT,
-            //     0,
-            //     5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
-            //     (2 * std::mem::size_of::<f32>()) as *const () as *const _,
-            // );
-            // gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);
-            gl.EnableVertexAttribArray(0 as gl::types::GLuint);
-            // gl.EnableVertexAttribArray(color_attrib as gl::types::GLuint);
-            
-            
-
-            Self {
-                program,
-                vao,
-                vbo,
-                gl,
-                texture,
-            }
-        }
-    }
-
-    pub fn draw(&self) {
-        // self.draw_with_clear_color(0.0, 0.0, 0.0, 0.0)
-        self.draw_with_clear_color()
-    }
-
-    pub fn draw_with_clear_color(
-        &self,
-        // red: GLfloat,
-        // green: GLfloat,
-        // blue: GLfloat,
-        // alpha: GLfloat,
-    ) {
-        unsafe {
-            self.gl.UseProgram(self.program);
-            self.gl.Enable(gl::BLEND);
-            self.gl.BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
-
-
-            self.gl.BindVertexArray(self.vao);
-            self.gl.BindBuffer(gl::ARRAY_BUFFER, self.vbo);
-
-            // self.gl.ClearColor(red, green, blue, alpha);
-            // self.gl.Clear(gl::COLOR_BUFFER_BIT);
-
-            self.gl.ActiveTexture(gl::TEXTURE0);
-
-            let vertices: [f32; 24] = [
-                0.0 - 0.5 + 0.2, 0.0 - 0.5, 0.0, 0.0,
-                0.0 - 0.5 + 0.2, 1.0 - 0.5, 0.0, 1.0,
-                1.0 - 0.5 - 0.2, 1.0 - 0.5, 1.0, 1.0,
-                0.0 - 0.5 + 0.2, 0.0 - 0.5, 0.0, 0.0,
-                1.0 - 0.5 - 0.2, 1.0 - 0.5, 1.0, 1.0,
-                1.0 - 0.5 - 0.2, 0.0 - 0.5, 1.0, 0.0,
-            ];
-
-            self.gl.BindTexture(gl::TEXTURE_2D, self.texture);
-
-            self.gl.BufferSubData(gl::ARRAY_BUFFER, 0, 24 * std::mem::size_of::<f32>() as gl::types::GLsizeiptr, vertices.as_ptr() as *const _);
-
-            self.gl.DrawArrays(gl::TRIANGLES, 0, 6);
-        }
-    }
-
-    pub fn resize(&self, width: i32, height: i32) {
-        unsafe {
-            self.gl.Viewport(0, 0, width, height);
-        }
-    }
-}
-
-impl Deref for Renderer {
-    type Target = gl::Gl;
-
-    fn deref(&self) -> &Self::Target {
-        &self.gl
-    }
-}
-
-impl Drop for Renderer {
-    fn drop(&mut self) {
-        unsafe {
-            self.gl.DeleteProgram(self.program);
-            self.gl.DeleteBuffers(1, &self.vbo);
-            self.gl.DeleteVertexArrays(1, &self.vao);
-        }
-    }
-}
-
-unsafe fn create_shader(
-    gl: &gl::Gl,
-    shader: gl::types::GLenum,
-    source: &[u8],
-) -> gl::types::GLuint {
-    let shader = unsafe { gl.CreateShader(shader) };
-    unsafe {
-        gl.ShaderSource(
-            shader,
-            1,
-            [source.as_ptr().cast()].as_ptr(),
-            std::ptr::null(),
-        );
-        gl.CompileShader(shader);
-    }
-    shader
-}
-
-fn get_gl_string(gl: &gl::Gl, variant: gl::types::GLenum) -> Option<&'static CStr> {
-    unsafe {
-        let s = gl.GetString(variant);
-        (!s.is_null()).then(|| CStr::from_ptr(s.cast()))
-    }
-}
-
-// // #[rustfmt::skip]
-// // static VERTEX_DATA: [f32; 15] = [
-// //     -0.5, -0.5,  1.0,  0.0,  0.0,
-// //      0.0,  0.5,  0.0,  1.0,  0.0,
-// //      0.5, -0.5,  0.0,  0.0,  1.0,
-// // ];
-
-// #[rustfmt::skip]
-// static VERTEX_DATA: [f32; 12] = [
-//     -0.5, -0.5,
-//     -0.5,  0.0,
-//      0.0, -0.5,
-
-//     -0.5,  0.0,
-//      0.0, -0.5,
-//      0.0,  0.0,
-
-// ];
-
-// // const VERTEX_SHADER_SOURCE: &[u8] = b"
-// // #version 100
-// // precision mediump float;
-
-// // attribute vec2 position;
-// // attribute vec3 color;
-
-// // varying vec3 v_color;
-
-// // void main() {
-// //     gl_Position = vec4(position, 0.0, 1.0);
-// //     v_color = color;
-// // }
-// // \0";
-
-const VERTEX_SHADER_SOURCE: &[u8] = b"
-#version 460 core
-layout (location = 0) in vec4 vertex;
-out vec2 TexCoords;
-
-void main()
-{
-    gl_Position = vec4(vertex.xy, 0.0, 1.0);
-    TexCoords = vertex.zw;
-}
-\0";
-
-// // const FRAGMENT_SHADER_SOURCE: &[u8] = b"
-// // #version 100
-// // precision mediump float;
-
-// // varying vec3 v_color;
-
-// // void main() {
-// //     gl_FragColor = vec4(v_color, 1.0);
-// // }
-// // \0";
-
-const FRAGMENT_SHADER_SOURCE: &[u8] = b"
-#version 460 core
-in vec2 TexCoords;
-out vec4 color;
-
-uniform sampler2D text;
-
-void main()
-{    
-    color = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
-    // color = vec4(1.0, 1.0, 1.0, 1.0);
-}  
-\0";
